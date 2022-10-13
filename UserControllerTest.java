67bdfc0fa608d239a7b49410b3ba43a547f6961e
Yejin <79991960+syj0396@users.noreply.github.com>
2022-10-13 23:42:00 +0900

    controller test code 수정

--- UserControllerTest.java
+++ UserControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,24 +18,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ExceptionCollector;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import skkuchin.service.domain.AppUser;
+import skkuchin.service.domain.Role;
+import skkuchin.service.exception.BlankException;
+import skkuchin.service.exception.DiscordException;
+import skkuchin.service.exception.DuplicateException;
 import skkuchin.service.service.UserService;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Transactional
-//@RunWith(SpringJUnit4ClassRunner.class) //spring-test에서 제공하는 단위 테스트를 위한 클래스 러너
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@Transactional
 @AutoConfigureMockMvc
 public class UserControllerTest {
     @Autowired
@@ -41,15 +52,14 @@ public class UserControllerTest {
     //@Autowired
     private MockMvc mock;
 
-    //@MockBean
-    //private UserService userService;
+    @Mock
+    private UserService userService;
     @Autowired
     private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setup() {
         mock = MockMvcBuilders.standaloneSetup(userController)
-                .setControllerAdvice(GlobalExceptionHandler.class)
                 .build();
     }
 
@@ -62,7 +72,7 @@ public class UserControllerTest {
         MockHttpServletRequestBuilder builder =
                 get("/api/users");
 
-        mock.perform(builder)
+        mock.perform(get("/api/users"))
                 .andExpect(status().isOk());
     }
 
@@ -90,9 +100,68 @@ public class UserControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isInternalServerError())
-                .andExpect(result -> assertThat(result.getResolvedException().getMessage())
-                        .isEqualTo("re_password_error"));
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof DiscordException))
+                .andExpect(result -> assertThat(result.getResolvedException().getMessage()).isEqualTo("re_password_error"));
     }
+
+    @Test
+    @DisplayName("[POST] /api/user/save 실패")
+    void nickname_중복_확인() throws Exception {
+        Collection<Role> roles = new ArrayList<>();
+        roles.add(new Role(1L, "ROLE_USER"));
+        //AppUser user1 = new AppUser(1L, "skku", "skku123", "1234", new ArrayList<>());
+        //Mockito.when(userService.saveUser(user1)).thenReturn(user1);
+        //userService.saveUser(user1);
+        //-> mock객체여서 바로 사라지는건가..? -> 아마 db랑 연결된게 아니고 입력 반환만 하니까.
+
+        String user1 = objectMapper.writeValueAsString(
+                new SignUpForm("", "skku", "1234", "1234"));
+
+        //form 성공적으로 저장
+        mock.perform(post("/api/user/save")
+                .content(user1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+/*
+        String user2 = objectMapper.writeValueAsString(
+                new SignUpForm("skku", "skku123", "1234", "1234"));
+
+        mock.perform(post("/api/user/save")
+                        .content(user2)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                //.andExpect(status().is2xxSuccessful());
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof DuplicateException))
+                .andExpect(result -> assertThat(result.getResolvedException().getMessage()).isEqualTo("duplicate_error"));
+*/
+    }
+
+    @Test
+    @DisplayName("[POST] /api/user/save 실패")
+    void 빈_값_확인() throws Exception { // 빈 값: ""
+        String user = objectMapper.writeValueAsString(
+                new SignUpForm("", "skku", "1234", "1234"));
+/*
+        mock.perform(post("/api/user/save")
+                        .content(user)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                //.andExpect(status().is5xxServerError())
+                .andExpect(result -> assertTrue(result.getResolvedException().getClass().isAssignableFrom(BlankException.class)))
+                .andExpect(result -> assertThat(result.getResolvedException().getMessage()).isEqualTo("blank_error"));
+   */
+        MvcResult result = mock
+                .perform(
+                        post("/api/user/save")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(user))
+                .andExpect(
+                        (rslt) -> assertTrue(rslt.getResolvedException().getClass().isAssignableFrom(BlankException.class))
+                )
+                .andReturn();
+    }
+
 
     @Test
     void addRoleToUser() {
@@ -108,13 +177,5 @@ public class UserControllerTest {
 
     @Test
     void getApiUser() {
-    }
-}
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Object> SampleException(RuntimeException e) {
-
     }
 }
